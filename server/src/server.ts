44a5156463ecfa41cef8599be09d8951a2a19a38
John Cedrick Garcia <icedgarcia@gmail.com>
2022-10-04 14:19:37 +0800

    Configure database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,31 @@
-import server from './app';
-import path from 'path';
 import dotenv from 'dotenv';
+import App from './app';
+import ProjectController from './controllers/project.controller';
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-// Initialize the server
-const port = process.env.PORT || 8000;
-server.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+process.on('uncaughtException', (error) => {
+  console.error('UNCAUGHT EXCEPTION');
+  console.log(error.name, error.message);
+  console.log(error.stack);
+
+  process.exit(1);
 });
+
+// Read the configuration file
+dotenv.config();
+
+// Verify the port number
+const port = Number(process.env.PORT) || 8000;
+const app = new App([new ProjectController()], port);
+
+// Pull and verify MongoDB credentials
+if (!process.env.DATABASE || !process.env.DATABASE_PASSWORD) {
+  throw new Error('Database access credentials are not provided');
+}
+
+// Connect to MongoDB
+const database = process.env.DATABASE;
+
+App.connectToMongoDB(database);
+
+// Start the Server
+app.listen();
